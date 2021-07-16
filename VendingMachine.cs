943e2dc98a86fe2d9ddf396c68041f6b60e58ff4
monty grant <monray@fireworkx.co.za>
2021-07-16 21:12:13 +0200

    Updates to the  Vending Machine class - method CountChangePossibilities - returned int total for Unit Test

--- VendingMachine.cs
+++ VendingMachine.cs
@@ -25,6 +25,12 @@ namespace CoinChangeProblem
         #endregion
 
         #region CalculateChange
+        /// <summary>
+        /// Calculate Change
+        /// </summary>
+        /// <param name="purchaseAmount"></param>
+        /// <param name="tenderAmount"></param>
+        /// <returns></returns>
         public List<int> CalculateChange(double purchaseAmount, double tenderAmount)
         {
             var change = new List<int>();
@@ -48,22 +54,31 @@ namespace CoinChangeProblem
         #endregion
 
         #region CountChangePossibilities
+
+        /// <summary>
+        ///  Unit Test - total count
+        /// </summary>      
+        /// <param name="tender"></param>
+        /// <param name="purch"></param>
+        /// <param name="coinDenominations"></param>
+        /// <returns></returns>
         public static int CountChangePossibilities(double purch, double tender, int[] coinDenominations)
         {
+            var total = 0;          
             var machine = new VendingMachine(coinDenominations);
             var purchaseAmount = purch; // amount the item cost
             var tenderAmount = tender; // amount the user input for the purchase
             var change = machine.CalculateChange(purchaseAmount, tenderAmount); // expect 65 cents
-            //var results = 0;
             List<int> results = new List<int>();
+
             for (int val = 0; val < change.Count; val++)
             {
-
-                results.Add(change[val]); 
-                Console.WriteLine($"Change[{val}] = {change[val]}");                
+                total += 1;                     
             }
 
-            return results[0];
+            return total;
+
+
         }
         #endregion
     }
