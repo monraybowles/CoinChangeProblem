bf3d3c58955c0a0557805e70ed9d12bd6399000d
monty grant <monray@fireworkx.co.za>
2021-07-16 19:33:10 +0200

    Updated class and the program cs file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
 namespace CoinChangeProblem
@@ -14,33 +15,36 @@ namespace CoinChangeProblem
         /// </summary>
         static void Main()
         {
+            int[] coinDenominationsPound = new int[] { 1, 2, 5, 10, 20, 50 }; // coin denominations – US Dollar
+            int[] coinDenominationsUSDollar = new int[] { 1, 5, 10, 25 }; // coin denominations – Pound
 
+            Array.Reverse(coinDenominationsPound);// reverse array for pound coins
+            Array.Reverse(coinDenominationsUSDollar);// reverse array for US coins
+            var machineDollar = new VendingMachine(coinDenominationsUSDollar);
+            var machinePound = new VendingMachine(coinDenominationsPound);
+            var purchaseAmount = 1.35; // amount the item cost
+            var tenderAmount = 2.00; // amount the user input for the purchase
+            var changeUSD = machineDollar.CalculateChange(purchaseAmount, tenderAmount); // expect 65 cents
+            var changePound = machinePound.CalculateChange(purchaseAmount, tenderAmount); // expect 65 cents
 
-            var change = new List<int>();
-            int[] coins = { 25, 10, 5, 1 };
-            int amount, count, i;
-            Console.Write("Enter the amount you want to change : ");
-            amount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("==========================");
-            for (i = 0; i < coins.Length; i++)
+            Console.WriteLine($"--------------------------------------------- USD "); // HEADER # 1
+            Console.WriteLine($"  ");
+            Console.WriteLine($"Total change amount (USD) = {machineDollar.Change}");
+            // Loop through data and write lines
+            for (int val = 0; val < changeUSD.Count; val++)
             {
-                count = amount / coins[i];
-                if (count != 0)
-                    for (int x = 0; x < count; x++)
-                    {
-                        change.Add(coins[i]);
-                    }
-              
-                amount %= coins[i];
+                Console.WriteLine($"Change[{val}] = {changeUSD[val]}");
             }
-
-            for (int val = 0; val < change.Count; val++)
+            Console.WriteLine($"  "); 
+            Console.WriteLine($"-------------------------------------------- POUND "); // HEADER # 2
+            Console.WriteLine($"  ");
+            // Loop through data and write lines
+            Console.WriteLine($"Total change amount (Pound) = {machinePound.Change}");
+            for (int val = 0; val < changePound.Count; val++)
             {
-                Console.WriteLine($"Change[{val}] = {change[val]}");
+                Console.WriteLine($"Change[{val}] = {changePound[val]}");
             }
-
             Console.ReadLine();
         }
-    
     }
 }
--- VendingMachine.cs
+++ VendingMachine.cs
@@ -1,18 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CoinChangeProblem
 {
-    class VendingMachine
+    public class VendingMachine
     {
-        private VendingMachine(coinDenominations)
+
+        #region Declarations 
+        private int[] coinDenominations;
+        public double Change { get; set; }
+
+        int i;
+
+        #endregion
+
+        #region VendingMachine
+        public VendingMachine(int[] coins)
         {
-            int[] coinDenominations;
-
-            return coinDenominations;
-
-
+            coinDenominations = coins;
         }
+
+        #endregion
+
+        #region CalculateChange
+        public List<int> CalculateChange(double purchaseAmount, double tenderAmount)
+        {
+            var change = new List<int>();
+            var changeAmount = tenderAmount - purchaseAmount;
+            changeAmount = Math.Round(changeAmount * 100, 2);
+            Change = changeAmount;
+            var count = 0;
+            for (i = 0; i < coinDenominations.Length; i++)
+            {
+                count = (int)changeAmount / coinDenominations[i];
+                if (count != 0)
+                    for (int x = 0; x < count; x++)
+                    {
+                        change.Add(coinDenominations[i]);
+                    }
+                changeAmount %= coinDenominations[i];
+            }
+            return change;
+        }
+
+        #endregion
+
+        #region CountChangePossibilities
+        public static int CountChangePossibilities(double purch, double tender, int[] coinDenominations)
+        {
+            var machine = new VendingMachine(coinDenominations);
+            var purchaseAmount = purch; // amount the item cost
+            var tenderAmount = tender; // amount the user input for the purchase
+            var change = machine.CalculateChange(purchaseAmount, tenderAmount); // expect 65 cents
+            //var results = 0;
+            List<int> results = new List<int>();
+            for (int val = 0; val < change.Count; val++)
+            {
+
+                results.Add(change[val]); 
+                Console.WriteLine($"Change[{val}] = {change[val]}");                
+            }
+
+            return results[0];
+        }
+        #endregion
     }
 }
+
